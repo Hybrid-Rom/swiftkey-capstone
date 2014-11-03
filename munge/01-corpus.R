206972862f9be37ed26333f768193fd7732624ad
Nick Allen <nick@nickallen.org>
2014-11-03 12:58:30 -0500

    Added ability to sample a certain percentage of the entire text corpus.

--- munge/01-corpus.R
+++ munge/01-corpus.R
@@ -1,10 +1,16 @@
 
+# what percentage of the corpus should be kept for training?
+p <- 0.05
+
 cache_if_missing ("corpus", {
     
     # create a corpus from the text input
-    files <- list.files ("data/en_US_sample/", pattern = "\\.txt$", full.names = TRUE)
+    files <- list.files ("data/en_US/", pattern = "\\.txt$", full.names = TRUE)
     lines <- unlist (lapply (files, readLines))
-    corpus <- Corpus(VectorSource (lines))
+    
+    # sample a subset of the lines
+    index <- as.logical (rbinom (n=length(lines), size=1, prob=p))
+    corpus <- Corpus(VectorSource (lines [index]))
     
     # clean the corpus 
     corpus <- tm_map(corpus, content_transformer (tolower))
