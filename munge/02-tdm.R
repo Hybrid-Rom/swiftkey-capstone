5b9d716d2ca0fc69d7cd07c93ba007cd168ed060
Nick Allen <nick@nickallen.org>
2014-11-03 10:38:18 -0500

    RWeka does not work when parallelized.  Bah!

--- munge/02-tdm.R
+++ munge/02-tdm.R
@@ -1,6 +1,8 @@
 
 cache_if_missing ("tdm", {
     
+    # http://stackoverflow.com/questions/17703553/bigrams-instead-of-single-words-in-termdocument-matrix-using-r-and-rweka
+    options (mc.cores = 1)
     
     # create the term document matrix
     tok <- function(x) RWeka::NGramTokenizer(x, RWeka::Weka_control(min = 2, max = 2))
