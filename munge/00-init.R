dd7c65d42727b9fb31e13d27bd0e12163c78e793
Nick Allen <nick@nickallen.org>
2014-11-03 10:37:14 -0500

    Removed the code that adds support for parallelization because this causes RWeka to blow up.

--- munge/00-init.R
+++ munge/00-init.R
@@ -2,12 +2,11 @@
 # contains all initialization logic
 #
 
-# inits the parallelization library (only if installed)
-if (require (doMC)) {
-    registerDoMC ()
-    print (sprintf ("doMC parallel backend loaded with '%s' worker(s)", getDoParWorkers ()))
+# unzip the data files, only if necessary
+txts <- list.files (path = "data", pattern = "\\.txt$", recursive = TRUE)
+if (length (txts) == 0) {
+    
+    zips <- list.files (path = "data", pattern = "\\.zip$", full.names = TRUE, recursive = TRUE)
+    for (zip in zips) unzip (zip, exdir = dirname (zip))    
 }
 
-# unzip the data files
-zips <- list.files (path = "data", pattern = "\\.zip$", full.names = TRUE, recursive = TRUE)
-for (zip in zips) unzip (zip, exdir = dirname (zip))
