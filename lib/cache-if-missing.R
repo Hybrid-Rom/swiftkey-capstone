2665b557347169151110ffa03f15cc915509d304
Nick Allen <nick@nickallen.org>
2014-11-03 12:57:48 -0500

    Added a switch to not cache the data set if required.

--- lib/cache-if-missing.R
+++ lib/cache-if-missing.R
@@ -2,12 +2,16 @@
 # if a data set does not already exist, create and
 # cache it.  if the data set already exists, do nothing.
 #
-cache_if_missing <- function (name, data) {
+cache_if_missing <- function (name, data, 
+                              cache_write = as.logical (config$cache_loading)) {
     
     if (!data.exists(name)) {
         
-        # create and cache the data set
+        # create the data set
         assign (name, data, inherits = TRUE)
-        cache (name)
+        
+        # cache the data set, if allowed
+        if (cache_write)
+            cache (name)
     }
 }
--- tests/cache-if-missing-tests.R
+++ tests/cache-if-missing-tests.R
@@ -1,16 +1,47 @@
 
-# setup
-if (data.exists ("foo")) rm (foo)
 
+# setup
+# remove any test data sets in memory
+delete <- ls (pattern = "test[0-9]+")
+if (length (delete) > 0)
+    rm (list = delete, envir = envir)
+
+# remove any cached data files
+cleanup <- list.files ("cache", pattern = "test.*\\.RData", full.names = TRUE)
+file.remove (cleanup)
+
+# test 1
 # the code block can explicitly assign a data set to the given name
-expect_false (data.exists ("foo"))
-cache_if_missing ("foo", { foo <- data.frame () })
-expect_true (data.exists ("foo"))
+expect_false (data.exists ("test1"))
+cache_if_missing ("test1", { test1 <- data.frame () })
+expect_true (data.exists ("test1"))
 
-# setup
-if (data.exists ("foo")) rm (foo)
-
+# test 2
 # the code block does not have to assign to the given name
-expect_false (data.exists ("foo"))
-cache_if_missing ("foo", { data.frame () })
-expect_true (data.exists ("foo"))
+expect_false (data.exists ("test2"))
+cache_if_missing ("test2", { data.frame () })
+expect_true (data.exists ("test2"))
+
+# test 3
+# do not write to the cache
+cache_if_missing ("test3", { data.frame () }, cache_write = FALSE)
+expect_false (file.exists ("cache/test3.RData"))
+
+# test 4
+# write to the cache
+expect_false (file.exists ("cache/test4.RData"))
+cache_if_missing ("test4", { data.frame () }, cache_write = TRUE)
+expect_true (file.exists ("cache/test4.RData"))
+
+# test 5
+# default behavior is to write to the cache
+expect_false (file.exists ("cache/test5.RData"))
+cache_if_missing ("test5", { data.frame () })
+expect_true (file.exists ("cache/test5.RData"))
+
+# clean-up
+cleanup ()
+
+
+
+
