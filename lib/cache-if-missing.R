5c0a855e0bb52cb52533f9c8c750876e3519e7f6
Nick Allen <nick@nickallen.org>
2014-11-04 00:26:35 +0000

    Added log messages

--- lib/cache-if-missing.R
+++ lib/cache-if-missing.R
@@ -1,3 +1,6 @@
+
+classname <- function (name) paste (class (get (name)), collapse = " ")
+
 #
 # if a data set does not already exist, create and
 # cache it.  if the data set already exists, do nothing.
@@ -7,11 +10,16 @@ cache_if_missing <- function (name, data,
     
     if (!data.exists(name)) {
         
-        # create the data set
+        # assign the data set to the given name
         assign (name, data, inherits = TRUE)
         
         # cache the data set, if allowed
-        if (cache_write)
-            cache (name)
+        if (cache_write) {
+            cache (name)   
+            message (sprintf ("saving to cache: %s [%s]", name, classname (name)))
+        }
+        
+    } else {
+      message (sprintf ("loaded from cache: %s [%s] loaded from cache", name, classname (name)))
     }
 }
