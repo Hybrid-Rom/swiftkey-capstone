459f52c106570e327a67ed78e6436e6e26be43d3
Nick Allen <nick@nickallen.org>
2014-11-05 19:45:32 +0000

    Added additional logging to help pass the time.

--- lib/cache-if-missing.R
+++ lib/cache-if-missing.R
@@ -9,17 +9,18 @@ cache_if_missing <- function (name, data,
                               cache_write = as.logical (config$cache_loading)) {
     
     if (!data.exists(name)) {
-        
+        message ("creating data set: ", name, " @ ", Sys.time())
+      
         # assign the data set to the given name
         assign (name, data, inherits = TRUE)
         
         # cache the data set, if allowed
         if (cache_write) {
             cache (name)   
-            message (sprintf ("saving to cache: %s [%s]", name, classname (name)))
+            message (sprintf ("saving to cache: %s [%s] @ %s", name, classname (name), Sys.time()))
         }
         
     } else {
-      message (sprintf ("loaded from cache: %s [%s] loaded from cache", name, classname (name)))
+      message (sprintf ("loaded from cache: %s [%s] @ %s", name, classname (name), Sys.time()))
     }
 }
