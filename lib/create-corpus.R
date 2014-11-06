738aa407de6284e9fc4075f82993ccc54569c592
Nick Allen <nick@nickallen.org>
2014-11-06 10:04:35 -0500

    Extracted the 'create_corpus' function.

---
+++ lib/create-corpus.R
@@ -0,0 +1,17 @@
+#
+# creates a corpus from a vector of text input and performs
+# all of the necessary text pre-processing.
+#
+create_corpus <- function (text) {
+    stopifnot (is.character (text))
+    stopifnot (length (text) > 0)
+    
+    corpus <- Corpus (VectorSource (text))
+    corpus <- tm_map (corpus, content_transformer (tolower))
+    corpus <- tm_map (corpus, removePunctuation)
+    corpus <- tm_map (corpus, removeNumbers)
+    corpus <- tm_map (corpus, stemDocument, language = "english")
+    corpus <- tm_map (corpus, stripWhitespace)
+    
+    return (corpus)
+}
