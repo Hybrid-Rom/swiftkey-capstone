a9364e39aa01bcf2b8e851059cf389403315a04f
Nick Allen <nick@nickallen.org>
2014-11-06 12:28:22 -0500

    Changed the way a hard limit is placed on the test data set.

--- munge/05-test-phrases.R
+++ munge/05-test-phrases.R
@@ -12,18 +12,26 @@ cache_if_missing ("test.phrases", {
     # exclude any duplicate phrases which could occur
     test.phrases <- test.phrases [ !duplicated (phrase) ]
     
+    # place a hard limit on the number of test phrases
+    limit <- 3000
+    if (nrow (test.phrases > limit))
+        test.phrases <- test.phrases [1:limit]
+    
     # split the test phrase into previous and next
     test.phrases [, prev_words := except_last_word (phrase), by = phrase]
     test.phrases [, next_word := last_word (phrase), by = phrase]
     
     # use the model to predict the next word
-    limit <- 500000
-    test.phrases [1:limit, next_word_hat := predict_next_word (prev_words, model), by = phrase]
+    test.phrases [, next_word_hat := predict_next_word (prev_words, model), by = phrase]
     
     # output a quick preview of test results
     correct <- nrow (test.phrases [next_word == next_word_hat])
-    total <- min (limit, nrow (test.phrases [!is.na(next_word_hat)]))
-    message (sprintf ("The model predicted %s accurately.", percent (correct / total)))
+    total <- nrow (test.phrases)
+    message (sprintf ("The ngram model predicted %s accurately.", percent (correct / total)))
 
+    # the null model simply predicts 'the', the most common word for each phrase
+    correct <- nrow (test.phrases [next_word == "the"])
+    message (sprintf ("The null model predicted %s accurately.", percent (correct / total)))
+    
     test.phrases    
 })
