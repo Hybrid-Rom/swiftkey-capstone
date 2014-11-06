640453520a7ed17368622f3a1fe20a5b4ee423e3
Nick Allen <nick@nickallen.org>
2014-11-06 10:59:19 -0500

    No need to create a corpus for the holdout test set

--- munge/02-corpus.R
+++ munge/02-corpus.R
@@ -1,9 +1,7 @@
 
 # create the training and test corpus
 cache_if_missing ("train.corpus", { create_corpus (train.lines) })
-cache_if_missing ("test.corpus", { create_corpus (test.lines) })
 
 # clean-up
 rm (train.lines)
-rm (test.lines)
 gc ()
