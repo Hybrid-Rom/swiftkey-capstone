c19a1ee3189d21af1d6e6823d18d483d763f02ce
Nick Allen <nick@nickallen.org>
2014-11-06 10:58:56 -0500

    Using extracted create_tdm and create_model functions in the munge process.

--- munge/03-tdm.R
+++ munge/03-tdm.R
@@ -1,29 +1,10 @@
 
-# exclude any ngrams with less than 'cutoff' occurances
-cutoff <- 3 
-
-#
-# creates a term document matrix from a corpus
-#
-create_tdm <- function (corpus, min_n, max_n) {
-  
-  # create the term document matrix with bi and tri-grams
-  tok <- function(x) RWeka::NGramTokenizer(x, RWeka::Weka_control(min = min_n, max = max_n))
-  tdm <- TermDocumentMatrix (corpus, control = list (tokenize = tok, 
-                                                     global   = c(cutoff, Inf)))
-  
-  return (tdm)
-}
-
 # http://stackoverflow.com/questions/17703553/bigrams-instead-of-single-words-in-termdocument-matrix-using-r-and-rweka
 options (mc.cores = 1)
 
 # create a tdm for training with 1-grams, 2-grams, and 3-grams
-cache_if_missing ("train.tdm", create_tdm (train.corpus, 1, 3))
-rm (train.corpus)
+cache_if_missing ("train.tdm", create_tdm (train.corpus))
 
-# create a tdm with only 4-grams to use for testing the model
-# the first 3 words provide the context, then the 4th word is predicted
-cache_if_missing ("test.tdm", create_tdm (test.corpus, 4, 4))
-rm (test.corpus)
+# clean-up
+rm (train.corpus)
 
