1b9f4b5268ac4064df2b2cc40d8e209280b11a7d
Nick Allen <nick@nickallen.org>
2014-11-10 22:32:09 +0000

    Added test for ending sentence with exclam.

--- tests/split-sentences-tests.R
+++ tests/split-sentences-tests.R
@@ -2,4 +2,5 @@
 
 
 expect_equal (length (split_sentences ("this is the first.   this is the second.")), 2)
-expect_equal (length (split_sentences ("this is the first.... this is the second.")), 2)
+expect_equal (length (split_sentences ("this is the first... this is the second.")), 2)
+expect_equal (length (split_sentences ("this is the first!   this is the second.")), 2)
