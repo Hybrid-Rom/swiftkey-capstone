ea638ad253bbd745c234aa685898fdf9337ce428
Nick Allen <nick@nickallen.org>
2014-11-10 14:27:50 +0000

    Using the 'stringi' package which is much faster.

--- lib/create-corpus.R
+++ lib/create-corpus.R
@@ -7,12 +7,13 @@ create_corpus <- function (text) {
     stopifnot (length (text) > 0)
     
     corpus <- Corpus (VectorSource (text))
-    corpus <- tm_map (corpus, content_transformer (tolower))
+    corpus <- tm_map (corpus, content_transformer (stri_trans_tolower))
+    corpus <- tm_map (corpus, content_transformer (remove_nonprint))    
     corpus <- tm_map (corpus, removePunctuation)
     corpus <- tm_map (corpus, removeNumbers)
     #corpus <- tm_map (corpus, stemDocument, language = "english")
     corpus <- tm_map (corpus, stripWhitespace)
-    corpus <- tm_map (corpus, content_transformer (str_trim))
+    corpus <- tm_map (corpus, content_transformer (stri_trim_both))
     
     return (corpus)
 }
