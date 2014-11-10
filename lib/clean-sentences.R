bf66a2813fd36bf1ed2c931c646afb6f5c503d90
Nick Allen <nick@nickallen.org>
2014-11-10 22:32:50 +0000

    Now correctly removing anything that is not alpha, numeric or space.

--- lib/clean-sentences.R
+++ lib/clean-sentences.R
@@ -5,9 +5,8 @@
 clean_sentences <- function (sentences) {
   
   sentences <- stri_trans_tolower (sentences)
-  sentences <- stri_replace_all_regex (sentences, "[[:punct:]]+", "")
+  sentences <- stri_replace_all_regex (sentences, "[^[:alnum:][:blank:]]+", " ")
   sentences <- stri_replace_all_regex (sentences, "[[:digit:]]+", "###")
-  sentences <- stri_replace_all_regex (sentences, "[^[:print:]]+", "")
   sentences <- stri_trim_both (sentences)
   
   return (sentences)
