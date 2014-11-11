c9daf878c7abb00a8456a6d55a375518a69c4a79
Nick Allen <nick@nickallen.org>
2014-11-11 10:25:43 -0500

    Each sentence can now be delimited with a start and/or end tag.

--- lib/clean-sentences.R
+++ lib/clean-sentences.R
@@ -2,12 +2,29 @@
 #
 # performs pre-processing on 1 or more sentences.
 #
-clean_sentences <- function (sentences) {
-  
-  sentences <- stri_trans_tolower (sentences)
-  sentences <- stri_replace_all_regex (sentences, "[^[:alnum:][:blank:]]+", " ")
-  sentences <- stri_replace_all_regex (sentences, "[[:digit:]]+", "###")
-  sentences <- stri_trim_both (sentences)
-  
-  return (sentences)
+clean_sentences <- function (sentences, start_tag = "^", end_tag = "$") {
+    
+    # lower case
+    sentences <- stri_trans_tolower (sentences)
+    
+    # remove anything that is not alpha, numeric or whitespace
+    sentences <- stri_replace_all_regex (sentences, "[^[:alnum:][:blank:]]+", " ")
+    
+    # replace all digits with a simple indicator flag
+    sentences <- stri_replace_all_regex (sentences, "[[:digit:]]+", "###")
+    
+    # add starting/ending tag to each sentence
+    sentences <- stri_paste (start_tag, sentences, end_tag, sep = " ")
+    
+    # trim whitespace
+    sentences <- stri_trim_both (sentences)
+    
+    return (sentences)
 }
+
+#
+# a useful function for when no starting or ending tags are needed
+#
+clean_sentences0 <- function (x) clean_sentences (x, start_tag = "", end_tag = "")
+
+
--- tests/clean-sentences-tests.R
+++ tests/clean-sentences-tests.R
@@ -1,13 +1,18 @@
 
+# sentences should be delimited with leading/trailing markers
+expect_equal (clean_sentences ("a test sentence"), "^ a test sentence $")
 
 # all text input should be lower cased
-expect_equal (clean_sentences ("LOWER CASE"), "lower case")
+expect_equal (clean_sentences0 ("LOWER CASE"), "lower case")
 
 # all numbers are replaced with ###
-expect_equal (clean_sentences ("foo123456"), "foo###")
+expect_equal (clean_sentences0 ("foo123456"), "foo###")
 
 # all punctuation is removed
-expect_equal (clean_sentences ("!punc-tu-ation's"), "punc tu ation s")
+expect_equal (clean_sentences0 ("!punc-tu-ation's"), "punc tu ation s")
 
 # leading/trailing whitespace should be trimmed
-expect_equal (clean_sentences ("  trim whitespace   "), "trim whitespace" )
+expect_equal (clean_sentences0 ("  trim whitespace   "), "trim whitespace" )
+
+# vectorized input
+expect_equal (clean_sentences0 (c(" FIRST.", "SECOND.")), c("first", "second"))
