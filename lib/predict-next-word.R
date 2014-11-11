7208ac6cd3bb5202bcde2318c753cfb6f4670200
Nick Allen <nick@nickallen.org>
2014-11-11 13:31:42 -0500

    Implemented predict next word.

--- lib/predict-next-word.R
+++ lib/predict-next-word.R
@@ -2,14 +2,14 @@
 #
 # given a vector of previous words, predict the next word.
 #
-predict_next_word <- function (phrase, model, n = 3) {
+predict_next_word <- function (phrase, ngrams, n = 3) {
     
     # sanity checks
     stopifnot (is.character (phrase))
     stopifnot (length (phrase) == 1)
     
     # clean the input phrase
-    clean_phrase <- clean_sentences (phrase)
+    clean_phrase <- clean_sentences (phrase, end_tag = "")
     
     # break the sentence into its component words
     previous <- unlist (strsplit (clean_phrase, split = " ", fixed = TRUE))
@@ -19,14 +19,14 @@ predict_next_word <- function (phrase, model, n = 3) {
     for (i in n:1) {
         
         # ensure there are enough previous words 
-        # for example, a trigram model needs 2 previous words
+        # for example, a trigram ngrams needs 2 previous words
         if (len >= i-1) {
             
             # grab the last 'i-1' words
             ctx <- tail (previous, i-1)
             ctx <- paste (ctx, collapse = " ")
             
-            prediction <- model [context == ctx, word]
+            prediction <- ngrams [context == ctx, .SD [which.max (p), word]]
             if (length (prediction) > 0) {
                 #message (sprintf ("%s-gram: '%s' -> '%s'", i, ctx, prediction))
                 break
--- tests/predict-next-word-tests.R
+++ tests/predict-next-word-tests.R
@@ -1,22 +1,28 @@
 
-# create a model for testing
-text <- c("Jack jumps highest of all.", "Jill falls down the hill.", "Jane rolls over the wall.")
-corpus <- create_corpus (text)
-tdm <- create_tdm (corpus, ngram_min = 1, ngram_max = 4)
-model <- create_model (tdm, cutoff = 0)
+# a simple corpus for testing
+sentences <- c("I am Sam.", "Sam I am.", "I do not like green eggs and ham.")
+sentences <- clean_sentences (sentences)
 
-# 4-gram model matches on a 4-gram
-expect_equal (predict_next_word ("Bill falls down the", model, n = 4), "hill")
+# trigrams
+trigrams <- create_ngrams (sentences, 3)
+ngram_probabilities (trigrams)
 
-# 4-gram model matches on a 3-gram
-expect_equal (predict_next_word ("Who thought that Jill falls", model, n = 4), "down")
+# bigrams
+bigrams <- create_ngrams (sentences, 2)
+ngram_probabilities (bigrams)
 
-# 4-gram model matches on a 2-gram
-expect_equal (predict_next_word ("Where does Jane", model, n = 4), "roll")
+# unigrams
+unigrams <- create_ngrams (sentences, 1)
+ngram_probabilities (unigrams)
 
-# 4-gram model matches on a 1-gram
-expect_equal (predict_next_word ("Go drive", model, n = 4), "the")
+# combine the ngramss
+ngrams <- rbindlist (list (trigrams, bigrams, unigrams))
 
-# 4-gram model would match on a 4-gram, but specifically limited to 2-grams
-expect_equal (predict_next_word ("Bill falls down the", model, n = 2), "hill")
+# match on a 3-gram
+expect_equal (predict_next_word ("I am", ngrams, n = 3), "sam")
 
+# match on a 2-gram
+expect_equal (predict_next_word ("I", ngrams, n = 3), "am")
+
+# match a 1-gram
+expect_equal (predict_next_word ("", ngrams, n = 3), "i")
