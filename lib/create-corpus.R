5bcd284787906136bc17622585d11856ecf75956
Nick Allen <nick@nickallen.org>
2014-11-13 10:50:14 -0500

    First draft of the project's Milestone Report.

--- lib/create-corpus.R
+++ lib/create-corpus.R
@@ -7,11 +7,12 @@ create_corpus <- function (text) {
     stopifnot (length (text) > 0)
     
     corpus <- Corpus (VectorSource (text))
-    corpus <- tm_map (corpus, content_transformer (stri_trans_tolower))
     corpus <- tm_map (corpus, content_transformer (remove_nonprint))    
+    corpus <- tm_map (corpus, content_transformer (stri_trans_tolower))
     corpus <- tm_map (corpus, removePunctuation)
     corpus <- tm_map (corpus, removeNumbers)
-    #corpus <- tm_map (corpus, stemDocument, language = "english")
+    corpus <- tm_map (corpus, removeWords, stopwords("english"))
+    corpus <- tm_map (corpus, stemDocument, language = "english")
     corpus <- tm_map (corpus, stripWhitespace)
     corpus <- tm_map (corpus, content_transformer (stri_trim_both))
     
