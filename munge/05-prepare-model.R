712b6de838c9d752cb1940a63f10f30aee478c37
Nick Allen <nick@nickallen.org>
2014-12-08 17:36:45 -0500

    Moved the prepare model functionality to the munge/ directory.

---
+++ munge/05-prepare-model.R
@@ -0,0 +1,21 @@
+#
+# prepares the ngram model for deployment
+#
+
+model <- ngrams
+
+# remove extraneous columns
+model [, phrase_count := NULL]
+model [, context_count := NULL]
+model [, logp := NULL]
+model [, phrase := NULL]
+
+# keep the top 5 most likely words for each context only
+model <- model [ order (context, -p)]
+model [, rank := 1:.N, by = context]
+model <- model [ rank < 6 ]
+
+# save the ngram model in the app directory
+save (list = "model", envir = .GlobalEnv, file = "app/ngrams.RData")
+rm (model)
+s
